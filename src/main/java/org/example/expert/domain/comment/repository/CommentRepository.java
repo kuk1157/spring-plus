fe99bbbbcac88e9f1d33accd912726e8e6f65405
kuk1157 <kuk1938@naver.com>
2024-11-13 15:55:47 +0900

    refactor: 레벨 2_7 N+1문제 - 댓글 조회시 USER의 테이블까지 조회되는 문제 해결

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+    // 레벨2_7 N+1문제 FETCHTYPE이 LAZY인데도 불필요한 쿼리가 조회될 경우 FETCH JOIN으로 해결
+    @Query("SELECT c FROM Comment c JOIN FETCH c.user WHERE c.todo.id = :todoId")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
 }
