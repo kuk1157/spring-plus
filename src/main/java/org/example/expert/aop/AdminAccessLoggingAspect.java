5efac051deef73bc5949b4d08ff3c1c4dec14dc4
kuk1157 <kuk1938@naver.com>
2024-11-13 13:57:56 +0900

    refactor: 레벨1_3 AOP 수정

--- src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
+++ src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
@@ -18,7 +18,10 @@ public class AdminAccessLoggingAspect {
 
     private final HttpServletRequest request;
 
-    @After("execution(* org.example.expert.domain.user.controller.UserController.getUser(..))")
+    // @After("execution(* org.example.expert.domain.user.controller.UserController.getUser(..))")
+
+    // 레벨 1_3 AOP 경로 수정
+    @After("execution(* org.example.expert.domain.user.controller.UserAdminController.changeUserRole(..))")
     public void logAfterChangeUserRole(JoinPoint joinPoint) {
         String userId = String.valueOf(request.getAttribute("userId"));
         String requestUrl = request.getRequestURI();
